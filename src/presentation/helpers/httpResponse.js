6e0e0c12e8b73d11a61c3ea35eba76c8ace9eab5
Jeferson Ryan <50426159+JefersonRyan@users.noreply.github.com>
2020-11-28 11:29:23 -0300

    Move error 401 to a helper method

--- src/presentation/helpers/httpResponse.js
+++ src/presentation/helpers/httpResponse.js
@@ -1,4 +1,5 @@
 const MissingParamError = require('./missingParamError')
+const UnauthorizedError = require('./unauthorizedError')
 
 module.exports = class HttpResponse {
   static badRequest (paramName) {
@@ -11,6 +12,13 @@ module.exports = class HttpResponse {
   static internalServerError () {
     return {
       statusCode: 500
+    }
+  }
+
+  static unauthorizedError () {
+    return {
+      statusCode: 401,
+      body: new UnauthorizedError()
     }
   }
 }
